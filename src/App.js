23dbf557c454be439cd0f0cf093540cc68d4ee94
inv-Karan <panditkaran0928@gmail.com>
2020-06-02 16:12:42 +0530

    functional and class component scheme with usage of UI update is explained

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import DetailsKaran from './details'
 import WC from '../src/welcome'
@@ -6,6 +6,17 @@ import TY from '../src/thankyou'
 import CT from '../src/count'
 import DCT from '../src/decount'
 import FRM from '../src/form'
+import  a, { name as b , add } from '../src/exportType'
+
+const Demo = (props) => {
+  // return (<div>{`welcome ${props.abc}`}</div>) functional component to retrieve data from class component and return result. Props for tag
+  return (<div>{props.abc}</div>)
+} 
+
+const Demo1 = (qwe) => {
+  //functional component to retrieve data from class component and return result. Simple function call for assigning direct value.
+  return <div>{`welcome ${qwe}`}</div>
+}
 
 class App extends Component{
   constructor(){
@@ -17,6 +28,14 @@ class App extends Component{
   render(){
   return (
     <div className="App">
+      -----
+      <Demo abc={'123'}/>
+      {/* tag */}
+      ----
+      {Demo1('123')}
+      {/* direct value */}
+      ------
+      {a}---{b}----{add}
       <WC name={'karan'} />
       <WC name={'karan1'} />
       <WC name={'karan1fdsf'} />
---
+++ src/exportType.js
@@ -0,0 +1,5 @@
+export const name = 'karan'
+export const add = 'BK park'
+const a = '123'
+
+export default a
